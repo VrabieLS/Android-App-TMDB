3081efa72fea30b5c1a93773bd526d83358cc07d
lucianstefanvrabie <lucian-stefan.vrabie@student.tuiasi.ro>
2022-05-05 14:15:16 +0300

    fragmente

--- app/src/main/java/com/example/tmdb/MainActivity.kt
+++ app/src/main/java/com/example/tmdb/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.tmdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.navigation.findNavController
+import com.example.tmdb.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //binding.bottomNavigationView
+        val navController=findNavController(R.id.fragmentContainerView)
+        binding.bottomNavigationView.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
+            return@OnItemSelectedListener when(item.itemId){
+                R.id.home -> {
+                    navController.navigate(R.id.homeFragment)
+                    true
+                }
+                R.id.movies -> {
+                    navController.navigate(R.id.movieFragment)
+                    true
+                }
+                R.id.tv_series -> {
+                    navController.navigate(R.id.TVSeriesFragment)
+                    true
+                }
+                else -> false
+            }
+            })
+        Log.d(TAG , " aaaaaaasdasdsadasd")
     }
 }
