55b40ee6266c3969d74cb69b7b5857ce60e77bbd
lucianstefanvrabie <lucian-stefan.vrabie@student.tuiasi.ro>
2022-05-05 14:19:57 +0300

    partea2

--- app/src/main/java/com/example/tmdb/MainActivity.kt
+++ app/src/main/java/com/example/tmdb/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     val TAG = "MainActivity"
+    //test
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
